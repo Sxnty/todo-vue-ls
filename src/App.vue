88b65719568a14a6a00fa67c525b6cd8c794999b
Santiago <sxntydev@gmail.com>
2023-06-06 17:16:39 -0300

    feat added task redering function

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,31 @@
 <script setup>
 import { ref, onMounted, computed, watch } from 'vue';
-const todos = ref([]);
+import {MdDeleteforever} from 'oh-vue-icons/icons'
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+
+let todos = ref([]);
 const name = ref('');
 
-const input_content = ref('');
+const input_title = ref('');
+const input_description = ref('');
 const input_category = ref(null);
 
 onMounted(() => {
   name.value = localStorage.getItem('name') || ''
+  if(todos.value.length === 0) {
+    todos.value = JSON.parse(localStorage.getItem('todos')) || []
+  }
+
 })
 
 const addTodo = () => {
-  if(input_content.value === '' || !input_category.value) {
+  if (input_title.value === '' || !input_category.value) {
     return
   }
   todos.value.push({
-    content: input_content.value,
-    category : input_category.value,
+    title: input_title.value,
+    category: input_category.value,
+    description: input_description.value,
     done: false,
   })
 }
@@ -24,8 +33,9 @@ watch(name, (newValue) => {
   localStorage.setItem('name', newValue);
 })
 watch(todos, (newVal) => {
+  console.log(todos)
   localStorage.setItem('todos', JSON.stringify(newVal))
-}, {deep:true})
+}, { deep: true })
 </script>
 
 <template>
@@ -40,7 +50,8 @@ watch(todos, (newVal) => {
       <h3>Create a task.</h3>
       <form @submit.prevent="addTodo">
         <h4>Task title</h4>
-        <input type="text" placeholder="leave the dog" v-model="input_content">
+        <input type="text" placeholder="Enter task title" v-model="input_title">
+        <textarea type="text" placeholder="Task description" v-model="input_description" />
         <h4>Pick a category</h4>
         <div class="options">
           <label>
@@ -59,6 +70,20 @@ watch(todos, (newVal) => {
         <input type="submit" value="Add task.">
       </form>
     </section>
+
+    <div>
+      <section class="todo-list" v-for="todo in todos" :key="todo.title">
+        <div class="todo-item">
+          <h1>{{ todo.title }}</h1>
+          <label>{{todo.description }}</label>
+          <span>
+          </span>
+        </div>
+      </section>
+    </div>
+
+
+
 
   </main>
 </template>
